c8b1a7681b253f472a334f76ecde1b7cf9959288
Coby Randquist <crandquist@vm01-walle.(none)>
2009-10-09 11:07:31 -0700

    Added history, changed file names, increased width

--- lib/qer/todo.rb
+++ lib/qer/todo.rb
@@ -5,33 +5,43 @@ module Qer
     end
 
     attr_accessor :queue
-    
-    def initialize(filename = File.expand_path("~/.binqueue"))
+    attr_accessor :history
+
+    def initialize(filename = File.expand_path("~/.qer-queue"))
       @filename  = filename
+      @history_filename = "#{filename}-history"
+
       self.queue = Marshal.load(file) rescue []
+      self.history = Marshal.load(history_file) rescue []
     end
 
     def file(mode = "r")
       File.new(@filename, mode)
     end
-    
+
+    def history_file(mode = "r")
+      File.new(@history_filename, mode)
+    end
+
     def size
       self.queue.size
     end
-    
+
     def returning(thing)
       yield
       thing
     end
-    
+
     def add(item)
       self.queue << [Time.now.to_s, item]
       write
       print "Adding: "+item
     end
-    
+
     def remove(index)
       returning(item = self.queue.delete_at(index)) do
+        self.history << [Time.now.to_s, item[0], item[1]]
+        write_history
         write
         print "Removed #{item.last}"
       end
@@ -57,8 +67,12 @@ module Qer
       Marshal.dump(self.queue, file("w+"))
     end
 
+    def write_history
+      Marshal.dump(self.history, history_file("w+"))
+    end
+
     def width
-      80
+      100
     end
 
     def title
@@ -68,42 +82,62 @@ module Qer
     def hl
       "".center(width, '-')
     end
-        
+
     def tf(t)
       Time.time_ago(Time.parse(t))
     end
-    
+
     def process_line(index, item)
+      item.size == 2 ? process_queue_line(index,item) : process_history_line(index,item)
+    end
+
+    def process_queue_line(index, item)
       time, task = item
       left       = "(#{index}) #{task}"
       right      = tf(time).rjust(width - left.length)
       right.insert(0, left)
     end
 
+    def process_history_line(index, item)
+      end_time, time, task = item
+      left      = "(#{index}) #{task}"
+      right     = "#{tf(time)} | #{tf(end_time)}".rjust(width-left.length)
+      right.insert(0, left)
+    end
+
     def print(string = nil)
+      dump self.queue, string
+    end
+
+    def print_history(string = nil)
+      dump self.history, string, "Stuff Completed"
+    end
+
+    def dump(queue, string, label = title)
       out = []
       out << string if(string)
-      out << title
+      out << label
       out << hl
-      if(self.queue.size > 0)
-        self.queue.each_index do |index|
-          out << process_line(index, self.queue[index])
+      if(queue.size > 0)
+        queue.each_index do |index|
+          out << process_line(index, queue[index])
         end
       else
-        out << "Nothing to do!"
+        out << "Nothing in this queue!"
       end
       out << hl
       puts out.join("\n") unless self.class.quiet
     end
-    
+
     def command(args)
       case(args.shift)
-      when /^a(dd)?/   : self.add(args.join(" "))     # qer add Some task 1
-      when /^r(emove)?/: self.remove(args.shift.to_i) # qer remove 0
-      when /^pu(sh)?/  : self.push(args.join(" "))    # qer push Some task 2
-      when /^po(p)?/   : self.pop                     # qer pop
-      when /^clear/    : self.clear                   # qer clear 
-      when /.*help/    : self.help                    # qer help
+      when /^a(dd)?/     : self.add(args.join(" "))     # qer add Some task 1
+      when /^r(emove)?/  : self.remove(args.shift.to_i) # qer remove 0
+      when /^pu(sh)?/    : self.push(args.join(" "))    # qer push Some task 2
+      when /^po(p)?/     : self.pop                     # qer pop
+      when /^clear/      : self.clear                   # qer clear
+      when /^h(istory)?/  : self.print_history           # qer history
+      when /.*help/      : self.help                    # qer help
       else self.print                                 # qer
       end
     end
@@ -126,9 +160,11 @@ Commands:
     `qer pop`
   clear - Clears the entire list
     `qer clear`
+  history - displays list of completed tasks
+    `qer history`
   help - Prints this message
     `qer help`
-#{hl}    
+#{hl}
       EOF
       puts string unless self.class.quiet
     end
