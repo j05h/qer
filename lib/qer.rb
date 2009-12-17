4a40653b0e4ad95b8d519b576c3834c83bbfe58b
Jacob Dunphy <jacob.dunphy@gmail.com>
2009-12-17 07:42:04 -0800

    Version bump to 0.2.0

--- lib/qer.rb
+++ lib/qer.rb
@@ -3,7 +3,7 @@ $:.unshift(File.dirname(__FILE__)) unless $:.include?(File.dirname(__FILE__)) ||
 require 'time'
 require 'qer/todo'
 module Qer
-  VERSION = '0.1.1'
+  VERSION = '0.2.0'
 end
 
 class Time
