27f498f02b9e12a5478d18c61027e53f4eff7e32
Jacob Dunphy <jacob.dunphy@gmail.com>
2010-02-01 14:40:56 -0800

    Improving test coverage by actually testing the output of qer.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,16 @@ require 'shoulda'
 require File.dirname(__FILE__) + '/../lib/qer'
 
 Qer::ToDo.quiet = true
+
+class Test::Unit::TestCase
+
+  def assert_output matcher
+    assert_match matcher, read_stdout
+  end
+  
+  def read_stdout
+    @output.rewind
+    @output.read
+  end
+
+end
